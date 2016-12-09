66cd847ab65805c93627edf6637d24ea848c27a3
Julian Vera <jjv1@williams.edu>
2016-12-09 06:09:20 -0500

    created PentaGram method for user input

---
+++ WordPredictor.java
@@ -0,0 +1,39 @@
+public class WordPredictor {
+
+
+    public void userPentaGram(String words){
+	
+	String first = "\1";
+	String second = "\2";
+	String third = "\3";
+	String fourth = "\4";
+
+	words = words.replace(".","");
+	// store words in string array to easily determine number of words
+	String[] splitWords = words.split(" ");
+
+	//determine if there are currently more than 5 words typed in
+	int numWords = splitWords.length;
+	if(numWords < 5) {
+	    if(numWords == 0){
+		PentaGram p = null;
+	    }
+	    if(numWords == 1){
+		PentaGram p = new PentaGram(first,second,third,fourth,splitWords[0]);
+	    } else if (numWords == 2){
+		PentaGram p = new PentaGram(second,third,fourth,splitWords[0],splitWords[1]);
+	    } else if (numWords == 3){
+		PentaGram p = new PentaGram(third,fourth,splitWords[0],splitWords[1],splitWords[2]);
+	    } else if (numWords == 4){
+		PentaGram p = new PentaGram(fourth,splitWords[0],splitWords[1],splitWords[2],splitWords[3]);
+	    }
+	} else if(numWords == 5){
+	    PentaGram p = new PentaGram(splitWords[0],splitWords[1],splitWords[2],splitWords[3],splitWords[4]);
+	} else {
+	    //just grab the last five words
+	    PentaGram p = new PentaGram(splitWords[numWords-5],splitWords[numWords-4],splitWords[numWords-3],splitWords[numWords-2],splitWords[numWords-1]);
+	}
+    }
+
+
+}
