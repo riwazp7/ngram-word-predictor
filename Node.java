7a2bd400b1bf7467d7092927f4763e5709bf537c
Riwaz Poudyal <rp7@williams.edu>
2016-12-09 04:20:46 -0500

    Added basic Node.java

--- Node.java
+++ Node.java
@@ -1,25 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * A Node object is going to have a parent, word and frequency
-**/
+ **/
 
-public class Node {
+public class Node implements Comparable<Node> {
 
-  String word;
-  int frequency;
-  Node parent;
+    // Max updates to the ArrayList before we sort it.
+    private final int SORT_THRESHOLD = 5;
 
-  class Node(Node parent, String word, Int frequency) {
-    this.parent = parent;
-    this.word = word;
-    this.frequency = frequency;
-  }
+    ArrayList<Node> children = new ArrayList<>();
+    HashMap<String, Integer> childrenIndex = new HashMap<>();
 
-  public String getWord() {
-  	return word;
-  }
-  
-  public Node parent() {
-    return parent;
-  }
+    // The number of updates to the ArrayList since we last sorted it.
+    int updates = 0;
+
+    // This node's word and it's frequency. Init count is always 1.
+    String word;
+    Integer count = 1;
+
+    int level;
+
+    public Node(String word, int level) {
+        this.word = word;
+        this.level = level;
+    }
+
+    private void sortArrayList() {
+        updates = 0;
+    }
+
+    public void add(PentaGram p) {
+        count += 1;
+        String nextWord = p.getWord(level + 1);
+        if (childrenIndex.containsKey(nextWord)) {
+            children.get(childrenIndex.get(nextWord)).add(p);
+        } else {
+            children.add(new Node(nextWord, level + 1));
+            childrenIndex.put(nextWord, children.size() - 1);
+        }
+        if (updates > SORT_THRESHOLD) {
+            sortArrayList();
+        }
+    }
+
+    @Override
+    public int compareTo(Node n) {
+        return n.count.compareTo(this.count);
+    }
+
+    // Use wisely
+    @Override
+    public String toString() {
+        String result = word + ":" + count + "\n";
+        for (Node node : children) {
+            result += node.toString();
+        }
+        return result + "\n\n";
+    }
 
 }
--- WordCount.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Riwaz on 12/9/16.
- */
-public class WordCount {
-}
