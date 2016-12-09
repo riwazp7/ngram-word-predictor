923ece2ee95a16d2d606c3862bd36b941322223a
Julian Vera <jjv1@williams.edu>
2016-12-09 02:26:48 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,25 @@
+/**
+ * A Node object is going to have a parent, word and frequency
+**/
+
+public class Node {
+
+  String word;
+  Int frequency;
+  Node parent;
+
+  class Node(Node parent, String word, Int frequency) {
+  	this.parent = parent;
+    this.word = word;
+    this.frequency = frequency;
+  }
+
+  public String getWord() {
+  	return word;
+  }
+  
+  public Node parent() {
+    return parent;
+  }
+
+}
