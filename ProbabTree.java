24fe836017f96a5cd4441d5dd125235e20262007
Julian Vera <jjv1@williams.edu>
2016-12-10 18:31:39 -0500

    ability to add to tree using string array of file names

--- ProbabTree.java
+++ ProbabTree.java
@@ -1,12 +1,18 @@
 /**
  * Created by Riwaz on 12/9/16.
  */
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class ProbabTree {
 
     Node root;
 
+    // Choose a max_level int
     public ProbabTree() {
-        root = new Node();
+        root = new Node(30);
     }
 
     public void add(PentaGram p) {
@@ -18,13 +24,27 @@ public class ProbabTree {
         return root.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // ******** TEST ***********
         ProbabTree tree = new ProbabTree();
+
+        String[] files = new String[]{"test.txt"};
+
+        WordPredictor grams = new WordPredictor(files);
+        ArrayList<LinkedList<PentaGram>> pentaGrams = grams.pentaGrams;
+
+        for(LinkedList<PentaGram> penta : pentaGrams){
+            for(PentaGram p : penta){
+                tree.add(p);
+            }
+        }
+
+        /**
         tree.add(new PentaGram(new String[] {"the","big","brown","fox","jumped"}));
         tree.add(new PentaGram(new String[] {"the","big","brown","fox","jumped"}));
         tree.add(new PentaGram(new String[] {"the","big","man","is","nice"}));
         tree.add(new PentaGram(new String[]{"a","big","man","is","nice"}));
+         **/
         System.out.println(tree);
     }
 
