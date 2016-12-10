cb6a7ec4036c0ec63dc2ac4b44a88a32b0aa48df
Julian Vera <jjv1@williams.edu>
2016-12-10 04:11:33 -0500

    WordPredictor now returns QuadGram

--- WordPredictor.java
+++ WordPredictor.java
@@ -1,40 +1,33 @@
 public class WordPredictor {
 
-    public void userPentaGram(String words){
-	
-	String first = "\1";
-	String second = "\2";
-	String third = "\3";
-	String fourth = "\4";
+    public QuadGram getQuadGram(String words) {
 
-	words = words.replace(".","");
-	// store words in string array to easily determine number of words
-	String[] splitWords = words.split(" ");
+		String filler = "\1";
 
-	//determine if there are currently more than 5 words typed in
-	int numWords = splitWords.length;
-	if(numWords < 5) {
-	    if(numWords == 0){
-		PentaGram p = null;
-	    }
-	    if(numWords == 1) {
-		PentaGram p = new PentaGram(new String[] {first,second,third,fourth,splitWords[0]});
-	    } else if (numWords == 2){
-		PentaGram p = new PentaGram(new String[] {second,third,fourth,splitWords[0],splitWords[1]});
-	    } else if (numWords == 3){
-		PentaGram p = new PentaGram(new String[] {third,fourth,splitWords[0],splitWords[1],splitWords[2]});
-	    } else if (numWords == 4){
-		PentaGram p = new PentaGram(new String[] {fourth,splitWords[0],splitWords[1],splitWords[2],splitWords[3]});
-	    }
-	} else if(numWords == 5){
-	    PentaGram p
-				= new PentaGram(new String[] {splitWords[0],splitWords[1],splitWords[2],splitWords[3],splitWords[4]});
-	} else {
-	    //just grab the last five words
-	    PentaGram p = new PentaGram(new String[] {splitWords[numWords-5],splitWords[numWords-4],
-				splitWords[numWords-3],splitWords[numWords-2],splitWords[numWords-1]});
+		words = words.replace(".", "");
+		// store words in string array to easily determine number of words
+		String[] splitWords = words.split(" ");
+
+		QuadGram p = null;
+		//determine if there are currently more than 4 words typed in
+		int numWords = splitWords.length;
+		if (numWords < 4) {
+			if (numWords == 0) {
+				p = null;
+			} else if (numWords == 1) {
+				p = new QuadGram(new String[]{filler, filler, filler, splitWords[0]});
+			} else if (numWords == 2) {
+				p = new QuadGram(new String[]{filler, filler, splitWords[0], splitWords[1]});
+			} else if (numWords == 3) {
+				p = new QuadGram(new String[]{filler, splitWords[0], splitWords[1], splitWords[2]});
+			}
+		} else {
+			//just grab the last four words
+			p = new QuadGram(new String[]{splitWords[numWords - 4], splitWords[numWords - 3],
+						splitWords[numWords - 2], splitWords[numWords - 1]});
+		}
+
+		return p;
 	}
-    }
-
 
 }
