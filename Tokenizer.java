0082952ff6ad333a2fbb12708f3ad79270165822
Julian Vera <jjv1@williams.edu>
2016-12-10 04:19:48 -0500

    small changes

--- Tokenizer.java
+++ Tokenizer.java
@@ -29,17 +29,17 @@ public class Tokenizer {
 			PentaGram p;
 	    	String[] words = sentences[i].split(" ");
 	    	for(int j = 0; j < words.length; j++){
-			if( j == 0 ){
-				p = new PentaGram(new String[] {filler,filler,filler,filler,words[j]});
-			} else if ( j == 1 ) {
-		    	p = new PentaGram(new String[] {filler,filler,filler,words[j-1],words[j]});
-			} else if ( j == 2 ) {
-		    	p = new PentaGram(new String[] {filler,filler,words[j-2],words[j-1],words[j]});
-			} else if ( j == 3 ){
-		    	p = new PentaGram(new String[] {filler,words[j-3],words[j-2],words[j-1],words[j]});
-			} else {
-				p = new PentaGram(new String[]{words[j - 4], words[j - 3], words[j - 2], words[j - 1], words[j]});
-			}
+				if( j == 0 ){
+					p = new PentaGram(new String[] {filler,filler,filler,filler,words[j]});
+				} else if ( j == 1 ) {
+		    		p = new PentaGram(new String[] {filler,filler,filler,words[j-1],words[j]});
+				} else if ( j == 2 ) {
+		    		p = new PentaGram(new String[] {filler,filler,words[j-2],words[j-1],words[j]});
+				} else if ( j == 3 ){
+		    		p = new PentaGram(new String[] {filler,words[j-3],words[j-2],words[j-1],words[j]});
+				} else {
+					p = new PentaGram(new String[]{words[j - 4], words[j - 3], words[j - 2], words[j - 1], words[j]});
+				}
 				result.add(p);
 	    	}
 		}
