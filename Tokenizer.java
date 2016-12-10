4a82cfc6e930cf93eed0b6dd104441348115eff6
Riwaz Poudyal <rp7@williams.edu>
2016-12-10 03:39:57 -0500

    Indentation in Tokenizer.java

--- Tokenizer.java
+++ Tokenizer.java
@@ -9,45 +9,44 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Tokenizer {
-  
-
-    String corpus = new String;
     
     public static LinkedList<PentaGram> tokenize(String filename)
 	          throws FileNotFoundException {
 	
-	Scanner fileScanner = new Scanner(new File(filename));
-	LinkedList<PentaGram> result = new LinkedList<PentaGram>();
-	//store all strings in result list
-	while (fileScanner.hasNextLine()) {	  
-	    corpus += fileScanner.nextLine();
-	}
+		Scanner fileScanner = new Scanner(new File(filename));
+		LinkedList<PentaGram> result = new LinkedList<>();
+		String corpus = "";
 
-	String[] sentences = corpus.split("\\.");
-	String first = "\1";
-	String second = "\2";
-	String third = "\3";
-	String fourth = "\4";
-
-	for(int i = 0; i < sentences.length; i++){
-	    String[] words = sentences[i].split(" ");
-	    for(int j = 0; j < words.length; j++){
-		if( j == 0 ){
-		    PentaGram p = new PentaGram(new String[] {first,second,third,fourth,words[j]});
-		} else if ( j == 1 ) {
-		    PentaGram p = new PentaGram(new String[] {second,third,fourth,words[j-1],words[j]});
-		} else if ( j == 2 ) {
-		    PentaGram p = new PentaGram(new String[] {third,fourth,words[j-2],words[j-1],words[j]});
-		} else if ( j == 3 ){
-		    PentaGram p = new PentaGram(new String[] {fourth,words[j-3],words[j-2],words[j-1],words[j]});
-		} else {
-		    PentaGram p = new PentaGram(new String[] {words[j-4],words[j-3],words[j-2],words[j-1],words[j]});
+		//store all strings in result list
+		while (fileScanner.hasNextLine()) {
+	    	corpus += fileScanner.nextLine();
 		}
-		result.add(p);
-	    }	    
+
+		String[] sentences = corpus.split("\\.");
+		String first = "\1";
+		String second = "\2";
+		String third = "\3";
+		String fourth = "\4";
+
+		for(int i = 0; i < sentences.length; i++){
+	    	String[] words = sentences[i].split(" ");
+	    	for(int j = 0; j < words.length; j++){
+			if( j == 0 ){
+		    	PentaGram p = new PentaGram(new String[] {first,second,third,fourth,words[j]});
+			} else if ( j == 1 ) {
+		    	PentaGram p = new PentaGram(new String[] {second,third,fourth,words[j-1],words[j]});
+			} else if ( j == 2 ) {
+		    	PentaGram p = new PentaGram(new String[] {third,fourth,words[j-2],words[j-1],words[j]});
+			} else if ( j == 3 ){
+		    	PentaGram p = new PentaGram(new String[] {fourth,words[j-3],words[j-2],words[j-1],words[j]});
+			} else {
+		    	PentaGram p = new PentaGram(new String[] {words[j-4],words[j-3],words[j-2],words[j-1],words[j]});
+			}
+			result.add(p);
+	    	}
+		}
+		return pentagram;
 	}
-	return pentagram;
-    }
     
     public static void main(String[] args) {
 	// TEST
