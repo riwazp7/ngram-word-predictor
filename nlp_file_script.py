705247e994639f224d495b6e0b59125a3e60d393
Julian Vera <jjv1@williams.edu>
2016-12-11 03:07:53 -0500

    added script to make desirable file.

---
+++ nlp_file_script.py
@@ -0,0 +1,38 @@
+'''
+Python Script for NLP final project
+Basically we only wants lines to end with periods.
+'''
+
+import re
+
+with open('words.txt', 'r') as rf:
+    with open('fixed.txt', 'w') as wf:
+
+        for line in rf:
+            #line = line.rstrip('\n')
+            # lines we want
+            if line.endswith(".\n"):
+                wf.write(line)
+
+            elif line.endswith("?\n"):
+                line = line.rstrip("?\n")
+                line = line + ".\n"
+                wf.write(line)
+
+            elif line.endswith("!\n"):
+                line = line.rstrip("!\n")
+                line = line + ".\n"
+                wf.write(line)
+
+            elif line.endswith(",\n"):
+                line = line.rstrip("\n")
+                wf.write(line+" ")
+
+            elif len(line) == 1:
+                wf.write(line.rstrip("\n"))
+            
+            else:
+                #line = line.rstrip(
+                line = line.rstrip("\n")
+                line = line + ".\n"
+                wf.write(line)
